9ca4f26dc245726590f7b959f7638bc3992fbf43
ruben <s236819@studenti.polito.it>
2021-03-29 14:43:40 +0200

    ' not private image' inserted dinamically

--- Html_stuff/WA1-Lab2/lab3.js
+++ Html_stuff/WA1-Lab2/lab3.js
@@ -90,15 +90,6 @@ for (let i=0 ; i < taskList.length(); i++){
     const input = document.createElement('input');
     const label = document.createElement('label');    
 
-    if (!tempTask.priv){
-        const html = '<svg class="bi bi-person-square" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
-        '<path fill-rule="evenodd" d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z" clip-rule="evenodd"/>'+
-        '<path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>'+
-      '</svg>';
-
-      div2.innerHTML= html;
-    }
-
 
     // Class assignment
     li.className = "list-group-item"; 
@@ -110,9 +101,7 @@ for (let i=0 ; i < taskList.length(); i++){
         label.className = "custom-control-label important";
     }else{
         label.className = "custom-control-label";  
-    }
-
-    
+    }  
     
     // Populate text field
     label.innerText = tempTask.description;
@@ -131,11 +120,15 @@ for (let i=0 ; i < taskList.length(); i++){
         div1.appendChild(small);
     } 
 
+    if (!tempTask.priv){
+        const html = '<svg class="bi bi-person-square" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
+        '<path fill-rule="evenodd" d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z" clip-rule="evenodd"/>'+
+        '<path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>'+
+      '</svg>';
 
-    //temp.className = "list-group-item";
-    //temp.innerText = taskList.getElementByIndex(i).description ; 
-    //task_list.appendChild(temp);
+      div2.insertAdjacentHTML("afterend" , html);
+    }
+
 }
 
-//task_list.appendChild(first_el);
  
