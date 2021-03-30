2bd236bf7d1e99f3a6a1290f6b19ad7ef59c6317
ruben <s236819@studenti.polito.it>
2021-03-30 11:10:45 +0200

    Lab3 - Point 1 completed

--- Html_stuff/WA1-Lab3/lab3.js
+++ Html_stuff/WA1-Lab3/lab3.js
@@ -91,7 +91,7 @@ for (let i=0 ; i < taskList.length(); i++){
     const label = document.createElement('label');    
 
 
-    // Class assignment
+    // Class assignment and attributes
     li.className = "list-group-item"; 
     div1.className = "d-flex w-100 justify-content-between";
     div2.className = "custom-control custom-checkbox";
@@ -102,7 +102,8 @@ for (let i=0 ; i < taskList.length(); i++){
         label.className = "custom-control-label important";
     }else{
         label.className = "custom-control-label";  
-    }  
+    }
+    label.for = `check-t${i+1}`;  
     
     // Populate text field
     label.innerText = tempTask.description;
@@ -115,13 +116,14 @@ for (let i=0 ; i < taskList.length(); i++){
     input.after(label);
 
     
-    if (tempTask.deadline != '<not defined>'){
-        const small = document.createElement('small');
-        small.innerText = tempTask.deadline;
-        div1.appendChild(small);
-    } 
+    //if (tempTask.deadline != '<not defined>'){
+    const small = document.createElement('small');
+    small.innerText = tempTask.deadline;
+    div1.appendChild(small);
+    //} 
 
-    if (!tempTask.priv){
+
+    if (!tempTask.private){
         const html = '<svg class="bi bi-person-square" width="1.2em" height="1.2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">'+
         '<path fill-rule="evenodd" d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z" clip-rule="evenodd"/>'+
         '<path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>'+
@@ -131,5 +133,8 @@ for (let i=0 ; i < taskList.length(); i++){
     }
 
 }
+
+// set the filters
+const aside = document.querySelector('aside div');
 
  
